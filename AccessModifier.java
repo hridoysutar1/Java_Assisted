9e87be026da0ebe3ee1d3252ddadaa481ee6377d
Hridoy Sutar <hridoysutar1@gmail.com>
2022-07-16 12:46:31 +0530

    initial commit

---
+++ AccessModifier.java
@@ -0,0 +1,7 @@
+package first_lesson;
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        System.out.println(5);
+    }
+}
---
+++ TypeCasting.java
@@ -0,0 +1,7 @@
+package first_lesson;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
