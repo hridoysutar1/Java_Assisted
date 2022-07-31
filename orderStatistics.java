291038dae14747f5b3c50f6d11e7673431760752
hridoysutar1 <83903052+hridoysutar1@users.noreply.github.com>
2022-08-01 00:18:49 +0530

    Add files via upload

---
+++ orderStatistics.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+import java.util.Arrays;
+
+public class orderStatistics {
+    public static void main(String[] args) {
+        int[] arr={23,63,32,67,12,0,86,34};
+        int k=4;
+
+
+    }
+}
